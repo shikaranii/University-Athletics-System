ad576a21f853fe8443d34ee89d385acb72d8d0ab
shikaranii <eramisshika@gmail.com>
2024-02-01 19:26:40 +0800

    shika committed, able to run on my end

--- src/athletes/athletes.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AthletesService } from './athletes.service';
-import { AthletesController } from './athletes.controller';
-
-@Module({
-  controllers: [AthletesController],
-  providers: [AthletesService],
-})
-export class AthletesModule {}
