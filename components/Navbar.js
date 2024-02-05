b4b052f9459aa57136105542095a92aa5572714f
seanchvz <seann.chvz@gmail.com>
2024-02-05 17:09:42 +0800

    Smaller Logo

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,10 +1,20 @@
 // components/Navbar.js
+import Image from 'next/image';
+
 const Navbar = () => {
     return (
       <nav>
          <main className="bg-white">
+         <div className="container mx-auto px-4 flex items-center">
+        <Image
+          src="/ateneo.png"
+          width={150}
+          height={150}
+          className="mr-4"
+        />
+
+      </div>
 <div className="flex flex-col items-start" >
-  <h1 className="text-2xl font-bold text-gray-800 font-poppins">University Athletics Office</h1>
   <div className="flex flex-col items-start">
     <a href="/Dashboard" className="py-2 px-4 text-blue-500 hover:text-blue-600">Dashboard</a>
     <a href="/profile" className="py-2 px-4 text-blue-500 hover:text-blue-600">Profile Management</a>
