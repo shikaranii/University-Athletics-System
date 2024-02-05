34c44ee30beb3937c5d32fa6d30f69802c44c92a
seanchvz <seann.chvz@gmail.com>
2024-02-05 16:22:00 +0800

    Added Components

--- app/page.js
+++ app/page.js
@@ -1,19 +1,15 @@
 import Image from "next/image";
+import Navbar from "../components/Navbar";
 
 
-export default function Home() {
+const Home = () => {
   return (
-    <main className="bg-white">
-<div className="flex flex-col items-start" >
-  <h1 className="text-2xl font-bold text-gray-800 font-poppins">University Athletics Office</h1>
-  <div className="flex flex-col items-start">
-    <a href="/Dashboard" className="py-2 px-4 text-blue-500 hover:text-blue-600">Dashboard</a>
-    <a href="/profile" className="py-2 px-4 text-blue-500 hover:text-blue-600">Profile Management</a>
-    <a href="/events" className="py-2 px-4 text-blue-500 hover:text-blue-600">Events</a>
-    <a href="/inventory" className="py-2 px-4 text-blue-500 hover:text-blue-600">Inventory</a>
-  </div>
-</div>
-
-    </main>
+    <div>
+      <Navbar />
+      <h1>University Athletics Office</h1>
+      {/* Other page content */}
+    </div>
   );
 }
+
+export default Home;
