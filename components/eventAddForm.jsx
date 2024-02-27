c193413adbb81b47a0a82b84f448ba5f34d4a727
seanchvz <seann.chvz@gmail.com>
2024-02-27 21:20:17 +0800

    frontend create event

--- components/eventAddForm.jsx
+++ components/eventAddForm.jsx
@@ -7,14 +7,14 @@ const EventAddForm = () => {
 <section className="bg-gray-100 py-8">
   <div className="container mx-auto">
     <h2 className="text-3xl font-semibold text-gray-800 mb-4">Create Sport Event</h2>
-    {/* Add your form or input fields here for adding student athletes */}
+   
   </div>
 </section>
 
 <section className="bg-gray-100 py-8">
   <div className="container mx-auto">
-    <div className="flex space-x-4"> {/* Use flex to create a row layout */}
-      <div className="flex flex-col space-y-4 w-1/2"> {/* Wrap the forms in a flex-col to stack them vertically and set width */}
+    <div className="flex space-x-4"> 
+      <div className="flex flex-col space-y-4 w-1/2"> 
         <div className="flex flex-col space-y-4">
           <input type="text" placeholder="Event Name*" className="input input-bordered w-full max-w-xs" />
           <input type="text" placeholder="Sport" className="input input-bordered w-full max-w-xs" />
@@ -23,7 +23,7 @@ const EventAddForm = () => {
           <input type="text" placeholder="Date" className="input input-bordered w-full max-w-xs" />
         </div>
         <div className="flex flex-col space-y-4">
-          <h1 className="text-xl font-semibold text-gray-800 mb-1 font-light">Players</h1> {/* Apply a thin font style */}
+          <h1 className="text-xl font-semibold text-gray-800 mb-1 font-light">Players</h1> 
           <input type="text" placeholder="Player" className="input input-bordered w-full max-w-xs" />
           <input type="text" placeholder="Player" className="input input-bordered w-full max-w-xs" />
           <input type="email" placeholder="Player" className="input input-bordered w-full max-w-xs" />
@@ -31,11 +31,11 @@ const EventAddForm = () => {
           <input type="text" placeholder="Player" className="input input-bordered w-full max-w-xs" />
         </div>
       </div>
-      <div className="flex flex-col space-y-4 w-1/2"> {/* Wrap the table in a flex-col and set width */}
+      <div className="flex flex-col space-y-4 w-1/2"> 
         <h2 className="text-2xl font-semibold text-gray-800 mb-2">Recently Added Events</h2>
       </div>
     </div>
-    <button type="submit" className="btn btn-primary mt-4">Add Event</button> {/* Add some margin to the button for spacing */}
+    <button type="submit" className="btn btn-primary mt-4">Add Event</button> 
   </div>
 </section>
 
