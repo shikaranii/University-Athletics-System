d793159819b9fc93c8a7d56de9f97b5d80a88cd9
seanchvz <seann.chvz@gmail.com>
2024-03-11 17:55:23 +0800

    Fixed Sports Errors

--- sia-2-athletics0-back/src/events/events.controller.ts
+++ sia-2-athletics0-back/src/events/events.controller.ts
@@ -22,10 +22,10 @@ export class EventsController {
     return this.eventsService.findOne(+id);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateEventDto: UpdateEventDto) {
-    return this.eventsService.update(+id, updateEventDto);
-  }
+  // @Patch(':id')
+  // update(@Param('id') id: string, @Body() updateEventDto: UpdateEventDto) {
+  //   return this.eventsService.update(+id, updateEventDto);
+  // }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
